e69572b71d8ceb3e54cd827006d2ebd5be612b82
Daryl Bennett <kd8bny@gmail.com>
2013-07-31 18:08:27 -0400

    Made modular
    Cut un-needed code. Made simple

--- lms/findLength.m
+++ lms/findLength.m
@@ -2,11 +2,10 @@
 %Use of SICK LMS 111 
 %Daryl W. Bennett ~dwbennet@mtu.edu ~kd8bny@gmail.com
 %Purpose: Function to calculate message length and reurn value to telegram
-
+%TODO make larger length values
 %% NOTES
 % MUST return HEX String cell array ~ie {'HEX'} length of 4
 %% Function Header
-function MSGlength = findLength(CMDtype,SPC,CMD,USRlvl,PSWRD)
-MSGlength = dec2hex(length(CMDtype)+length(CMD)+2*length(SPC)+length(USRlvl)+length(PSWRD),2);
-MSGlength = {'00','00','00',MSGlength}
+function teleLength = findLength(MSGlength)
+teleLength = {'00','00','00',dec2hex(MSGlength)};
 end
