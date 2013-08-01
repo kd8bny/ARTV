f47a548befe016264e8b438cc19302d610bb7c39
Daryl Bennett <kd8bny@gmail.com>
2013-08-01 12:00:38 -0400

    derp

--- lms/CHKSUM.m
+++ lms/CHKSUM.m
@@ -15,10 +15,10 @@ MSGDec = zeros(1,L_MSG);
 %xor
 lastBin = 00000000; %for xor function first run
 for(i=1:L_MSG)
-    MSGDec(i) = hex2dec(MSG(i));
+    MSG(i)
+    MSGDec = hex2dec(MSG(i));
     %Grab 8'b
-    %MSGBin = decimalToBinaryVector(MSGDec(i),8,'MSBFirst');
-    MSGBin = de2bi(MSGDec(i),8,'left-msb');
+    MSGBin = de2bi(MSGDec,8,'left-msb');
     lastBin = xor(MSGBin,lastBin);
 end
 %Convert back to hex
