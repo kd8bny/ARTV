033f458fab11b959b154fb0c695c23b165df3aaa
Daryl Bennett <kd8bny@gmail.com>
2013-08-01 10:46:16 -0400

    Init Commit
    Function to get freq and angle res

---
+++ lms/LMPscancfg.m
@@ -0,0 +1,33 @@
+%% INFO
+%Use of SICK LMS 111 
+%Daryl W. Bennett ~dwbennet@mtu.edu ~kd8bny@gmail.com
+%Purpose: Function to Get Frequency and Angular Resolution
+%TODO: Make function
+
+%Notes: All binary (Could use HEX in future)
+%command structure:
+%[Start text][MSG length][CMD Type][SPC][CMD][CHKSUM]
+
+%% Setup
+clc,clear
+%Delclare telegram. See: INFO/command structure
+telegramCell = {};
+STX = {'02','02','02','02'};
+SPC = {'20'};
+
+%predefined: (See INFO/command structure)
+CMDtype = {'73','52','4E'};
+CMD = {'4C','4D','50','73','63','61','6E','63','66','67'};
+
+%% Set Telegam
+% See: INFO/command structure
+telegramCell(1:4) = STX(1:4);
+%compute telegramCell(5:8) last for modular code
+telegramCell(9:11) = CMDtype(1:3);
+telegramCell(12) = SPC(1);
+telegramCell(13:22) = CMD(1:10);
+telegramCell(23) = {CHKSUM(telegramCell(9:22))};  %Check Sum
+
+telegramCell(5:8) = findLength(length(telegramCell(9:22)))
+
+%%now to send telegram
