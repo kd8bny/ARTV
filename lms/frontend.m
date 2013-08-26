02e3ab4c37de74884ac6fb39f8f025b7c6e4e22e
Daryl Bennett <kd8bny@gmail.com>
2013-08-26 15:32:27 -0400

    More global

--- lms/frontend.m
+++ lms/frontend.m
@@ -4,7 +4,7 @@
 %Purpose: Frontend to all functions
 
 %Notes: 
-
+%Change IP and port here
 %% Setup
 clc,clear
 
@@ -13,13 +13,12 @@ fprintf('Welcome to LIDAR Communication frontend ~dwbennet@mtu.edu ~kd8bny@gmail
 fprintf('\nUse with LMS11x and LMS 15x \n')
 fprintf('For use instructions see README.MD\n\n')
 
-%Entry
-global udpstate entry
-entry = 99;
-udpstate = 0;
+%Global Values
+global udpstate entry IP port
+entry = 'na'; udpstate = 0; IP = '169.254.43.166'; port = 2111;
 
 while(entry~=0)
-    fprintf('Commands:\n\t 1:Log in\n\t 2:Set Freq & Res\n\t 3:Configure Scan Content\n\t 4:Configure Scan Output\n\t 5:Store Parameters\n\t 6:Log out\n\t 7:Request Scan\n\n 0:QUIT\n')
+    fprintf('Commands:\n\t 1:Log in\n\t 2:Set Freq & Res\n\t 3:Configure Scan Content\n\t 4:Configure Scan Output\n\t 5:Store Parameters\n\t 6:Log out\n\t 7:Request Scan\n\t 8:Settings\n\n 0:QUIT\n')
     entry = input('Command Entry:');
     if(entry==1)
         sMN_setAccessMode
@@ -43,12 +42,16 @@ while(entry~=0)
         fprintf('Try also sEM_**')
         sRN_LMDscandata
         %sEN_LMDscandata
+    elseif(entry==8)
+        IP = input('IP address of LMS 1xx: ','s');
+        port = input('Port of LMS 1xx: ');
     else
         fprintf('Please enter valid command')
         pause(1)
     end
     %Prep for new command
-    entry = 99;
+    entry = 'na';
+    clc
     fprintf('Next Command?\n\n')
 end
 fprintf('Thanks for using')
